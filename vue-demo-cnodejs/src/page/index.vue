5f2058f02a1e5724cac62e99c7f4327ee40bbaa9
TCT <824026740@qq.com>
2017-12-04 18:04:25 +0800

    测试

---
+++ vue-demo-cnodejs/src/page/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>index page</div>
+</template>
+<script>
+export default {
+  created () {
+    this.$api.get('topics', null, r => {
+       console.log(r)
+      console.log("打印="+JSON.stringify(r))
+    })
+  }
+}
+</script>
